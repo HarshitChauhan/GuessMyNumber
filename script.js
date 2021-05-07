a2b509240dba8b0befcd0fa33a18642c8269ff0b
HarshitChauhan <harshitkumar2503@gmail.com>
2021-05-07 23:00:52 +0530

    implemented highscore logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
-const SecretNumber = Math.trunc(Math.random()*20)+1;
+let SecretNumber = Math.trunc(Math.random()*20)+1;
 let score=20;
+let highscore=0;
 document.querySelector('.check').addEventListener('click', function(){
     const guess=Number(document.querySelector('.guess').value);
     
@@ -14,6 +15,11 @@ document.querySelector('.check').addEventListener('click', function(){
 		document.querySelector('.number').textContent=SecretNumber;
         document.querySelector('body').style.backgroundColor='#60b347';
         document.querySelector('.number').style.width='30rem';
+        
+        if(score>highscore){
+            highscore=score;
+            document.querySelector('.highscore').textContent=highscore;
+        }
     } 
 	//guess is high
     else if(guess > SecretNumber){
