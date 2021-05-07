c6c8e70675fa75be58d29e31aa030d22a8c1720f
HarshitChauhan <harshitkumar2503@gmail.com>
2021-05-07 22:33:42 +0530

    Game logic implemented

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 "use strict";
+const SecretNumber = Math.trunc(Math.random()*20)+1;
+let score=20;
+document.querySelector('.check').addEventListener('click', function(){
+    const guess=Number(document.querySelector('.guess').value);
+    
+	//no input
+    if(!guess){
+        document.querySelector('.message').textContent='⛔ No number!';
+    } 
+	//player wins
+    else if(guess===SecretNumber){
+        document.querySelector('.message').textContent='🎉 Correct Number!';
+		document.querySelector('.number').textContent=SecretNumber;
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width='30rem';
+    } 
+	//guess is high
+    else if(guess > SecretNumber){
+        if(score>1){
+            document.querySelector('.message').textContent='📈 Too High!';
+            score--;
+            document.querySelector('.score').textContent=score;
+        }else{
+            document.querySelector('.message').textContent='💥 You lost the game!';
+            document.querySelector('.score').textContent=0;
+        }
+    } 
+	//guess is low
+    else if(guess < SecretNumber){
+        if(score>1){
+            document.querySelector('.message').textContent='📉 Too Low!';
+            score--;
+            document.querySelector('.score').textContent=score;
+        }else{
+            document.querySelector('.message').textContent='💥 You lost the game!';
+            document.querySelector('.score').textContent=0;
+        }
+    }
+})
